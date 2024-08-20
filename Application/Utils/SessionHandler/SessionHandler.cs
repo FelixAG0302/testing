7a7e792f6e035ea7d72819f73eb8915c4ab65e41
fguzman <fguzman@IBSCOMP124>
2024-08-20 10:15:48 -0400

    Interfaces, servicios, serializacion y SessionHandler

---
+++ Application/Utils/SessionHandler/SessionHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Text;
+
+
+namespace testing.Application.Utils.SessionHandler
+{
+    public static class SessionHandler
+    {
+        public static void Set<Value>(this ISession session, Value value, string Key)
+        {
+            string ValueToBeSave = JsonConvert.SerializeObject(value);
+            session.SetString(Key, ValueToBeSave);
+        }
+        public static Value Get<Value>(this ISession session, string key)
+        {
+            string value = session.GetString(key);
+            Value returnValue = value is null ? default : JsonConvert.DeserializeObject<Value>(value);
+            return returnValue;
+        }
+    }
+}
